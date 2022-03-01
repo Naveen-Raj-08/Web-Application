e31792c7629d9a447f6ff752420f0a7e091e09de
Naveen raj.K <47858683+Naveen-Raj-08@users.noreply.github.com>
2022-03-02 00:03:55 +0530

    Removed the unwanted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Sass/style.scss";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./Components/Home";
-import { Series } from "./Components/Series";
-import { Stories } from "./Components/Stories";
-import { NotFound } from "./Components/NotFound";
 import { Signup } from "./Components/Signup";
 import { Login } from "./Components/Login";
 
@@ -15,10 +11,6 @@ function App() {
         <Route exact path="/" element={<Login />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/Home" element={<Home />} />
-        <Route exact path="/stories" element={<Stories />} />
-        <Route exact path="/series" element={<Series />} />
-        <Route exact path="*" element={<NotFound />} />
       </Routes>
     </>
   );
--- src/Components/NotFound.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const NotFound = () => {
-  return <div className="text-danger text-center display-1">404 NotFound</div>;
-};
